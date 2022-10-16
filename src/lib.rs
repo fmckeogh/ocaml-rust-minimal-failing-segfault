d379707f6d35957ce93e28795bed39b858b29003
Ferdia McKeogh <ferdia@mckeogh.tech>
2022-10-16 19:14:50 +0100

    Remove lazy-mutex

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,4 @@
-#![deny(missing_docs)]
-
-//! Rust interface to `Sail` compiler library
-
-use {
-    ocaml::{List, Runtime, ToValue, Value},
-    once_cell::sync::Lazy,
-    parking_lot::Mutex,
-};
-
-/// OCaml runtime handle, initialised on first access
-///
-/// *Every* function referencing RT must either begin with RT.write() or it will possibly not be
-/// initialised and cause a "boxroot is not setup" error. This error will be hard to diagnose as
-/// it will be dependent on the order that other (correctly dereferencing RT and thus initialising
-/// the runtime) functions are called. Need to investigate how this can be prevented.
-static RT: Lazy<Mutex<Runtime>> = Lazy::new(|| Mutex::new(ocaml::runtime::init()));
+use ocaml::{List, ToValue, Value};
 
 ocaml::import! {
     fn internal_util_dedup(l: List<Value>) -> List<i32>;
@@ -22,18 +6,15 @@ ocaml::import! {
 
 /// Removes duplicate values in the supplied Vec
 pub fn dedup(list: Vec<i32>) -> Vec<i32> {
-    Lazy::force(&RT);
+    let rt = ocaml::runtime::init();
 
     let mut l = List::empty();
 
     for element in list {
-        let rt = RT.lock();
-        l = unsafe { l.add(&*rt, &element.to_value(&*rt)) };
+        l = unsafe { l.add(&rt, &element.to_value(&rt)) };
     }
 
-    unsafe { internal_util_dedup(&*RT.lock(), l) }
-        .unwrap()
-        .into_vec()
+    unsafe { internal_util_dedup(&rt, l) }.unwrap().into_vec()
 }
 
 #[cfg(test)]
@@ -42,8 +23,8 @@ mod tests {
 
     #[test]
     fn fail() {
-        for _ in 0..10 {
-            std::thread::spawn(|| loop {
+        for _ in 0..100 {
+            std::thread::spawn(|| {
                 let i = vec![-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
                 let o = dedup(i.clone());
                 assert_eq!(i, o);
